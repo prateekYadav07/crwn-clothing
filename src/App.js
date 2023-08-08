5919d97161fd0ac28a7bea4b4a8cad77ee650ec2
prateekYadav07 <yadavprateek39@gmail.com>
2023-08-08 23:02:51 +0530

    storing user information to firestore firebase

--- src/App.js
+++ src/App.js
@@ -19,9 +19,20 @@ class App extends Component {
   unsubscribeFromAuth = null;
 
   componentDidMount() {
-    this.unsubscribeFromAuth = auth.onAuthStateChanged(async (user) => {
-      this.setState({ currentUser: user });
-      createUserProfileDocument(user)
+    this.unsubscribeFromAuth = auth.onAuthStateChanged(async (userAuth) => {
+      if (userAuth) {
+        const userRef = await createUserProfileDocument(userAuth);
+        userRef.onSnapshot((snapshot) => {
+          this.setState({
+            currentUser: {
+              id: snapshot.id,
+              ...snapshot.data(),
+            },
+          });
+        });
+      } else this.setState({ currentUser: userAuth });
+      // this.setState({ currentUser: user });
+      // createUserProfileDocument(user)
     });
   }
 
