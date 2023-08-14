3cd0b114edeedac644ee425b92c51eb3630616d6
prateekYadav07 <yadavprateek39@gmail.com>
2023-08-14 11:50:52 +0530

    cartIcon, cartDropDown cmp and cart reducer

--- src/components/header/header.component.jsx
+++ src/components/header/header.component.jsx
@@ -5,8 +5,9 @@ import { Link } from "react-router-dom/cjs/react-router-dom.min";
 import { ReactComponent as Logo } from "../../assets/crown.svg";
 import { auth } from "../../firebase/firebase.utils";
 import CartIcon from "../cart-icon/cart-icon.components";
+import CartDropdown from "../cart-dropdown/cart-dropdown.components";
 
-const Header = ({ currentUser }) => {
+const Header = ({ currentUser, hidden }) => {
     console.log(currentUser);
     return (
         <div className="header">
@@ -21,12 +22,16 @@ const Header = ({ currentUser }) => {
                     : <Link className="option" to="/signin">SIGN IN</Link>}
                 <CartIcon />
             </div>
+            {
+                hidden ? null : <CartDropdown />
+            }
         </div>
     )
 }
 
-const mapStateToProps = state => ({
-    currentUser: state.user.currentUser
+const mapStateToProps = ({ user: { currentUser }, cart: { hidden } }) => ({
+    currentUser,
+    hidden
 })
 
 export default connect(mapStateToProps)(Header)
