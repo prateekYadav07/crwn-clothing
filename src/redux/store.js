1a9064d4081465d5014851955a4972fb1f19c6e3
prateekYadav07 <yadavprateek39@gmail.com>
2023-08-15 17:03:05 +0530

    added redux-persist for localstorage

--- src/redux/store.js
+++ src/redux/store.js
@@ -1,7 +1,19 @@
 import { legacy_createStore, applyMiddleware } from "redux";
 import logger from "redux-logger";
 import rootReducer from "./root-reducer";
+import persistStore from "redux-persist/es/persistStore";
 
 const middlewares = [logger];
 
-export const store = legacy_createStore(rootReducer, applyMiddleware(...middlewares))
+export const store = legacy_createStore(
+  rootReducer,
+  applyMiddleware(...middlewares)
+);
+export const persistor = persistStore(store);
+
+const expObj = {
+  store,
+  persistor,
+};
+
+export default expObj;
