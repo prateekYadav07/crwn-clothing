82b3d4c0d033c611ec46e161dbe3be491fe80382
prateekYadav07 <yadavprateek39@gmail.com>
2023-09-05 23:07:24 +0530

    collection preview routing fixed

--- src/components/collection-preview/collection-preview.component.jsx
+++ src/components/collection-preview/collection-preview.component.jsx
@@ -6,7 +6,7 @@ import { withRouter } from "react-router-dom/cjs/react-router-dom.min";
 const CollectionPreview = ({ title, items, history, match }) => {
     return (
         <div className="collection-preview">
-            <h1 className="title" onClick={() => { history.push(`${match.url}/${items.routeName}`) }}>{title.toUpperCase()}</h1>
+            <h1 className="title" onClick={() => { history.push(`${match.url}/${title.toLowerCase()}`) }}>{title.toUpperCase()}</h1>
             <div className="preview">
                 {
                     items
