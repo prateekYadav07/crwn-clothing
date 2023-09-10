562a8bd21c7c91f710a1b7fd36f7189e642aee98
prateekYadav07 <yadavprateek39@gmail.com>
2023-09-10 14:13:51 +0530

    refactored with spinner on collection pages to avoid colllection is null error

--- src/pages/collection/collection.pages.jsx
+++ src/pages/collection/collection.pages.jsx
@@ -1,10 +1,11 @@
 import React from "react";
 import './collection.styles.scss'
 import { connect } from "react-redux";
-import { selectCollection } from "../../redux/shop/shop.selectors";
+import { selectCollection, selectIsCollectionLoaded } from "../../redux/shop/shop.selectors";
 import CollectionItem from "../../components/collection-item/collection-item.components"
+import WithSpinner from "../../components/with-spinner/with-spinner.components";
 
-const CollectionPage = ({ collection }) => {
+const CollectionView = ({ collection }) => {
     const { title, items } = collection
     return (
         <div className="collection-page">
@@ -18,8 +19,18 @@ const CollectionPage = ({ collection }) => {
     )
 }
 
+const CollectionViewWithSpinner = WithSpinner(CollectionView)
+
+const CollectionPage = (props) => {
+    const { isCollectionLoaded } = props
+    return (
+        <CollectionViewWithSpinner isLoading={isCollectionLoaded} {...props} />
+    )
+}
+
 const mapStateToProps = (state, ownProps) => ({
-    collection: selectCollection(ownProps.match.params.collection)(state)
+    collection: selectCollection(ownProps.match.params.collection)(state),
+    isCollectionLoaded: selectIsCollectionLoaded(state)
 })
 
 export default connect(mapStateToProps)(CollectionPage)
