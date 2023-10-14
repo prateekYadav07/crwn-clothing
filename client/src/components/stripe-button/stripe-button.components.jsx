c00ca360ef77392132640f440595ad5e920feedd
prateekYadav07 <yadavprateek39@gmail.com>
2023-10-14 15:12:09 +0530

    created a server for stripe payment

--- client/src/components/stripe-button/stripe-button.components.jsx
+++ client/src/components/stripe-button/stripe-button.components.jsx
@@ -1,13 +1,26 @@
 import React from "react";
 import StripeCheckout from "react-stripe-checkout";
+import axios from "axios"
 
 const StripeCheckoutButton = ({ price }) => {
     const priceForStripe = price * 100
     const publishableKey = "pk_test_51Nh3l7SD9DUYhrhxOZKxxX7qqsgIbllxqALdYBwKcDzzjClSIzdmyoJ9jRLGQWaBN0wnok6pKydxctALUq2t2Zmq00AoBOvBWD"
 
     const onToken = (token) => {
-        console.log(token);
-        alert('payment succesful')
+        axios({
+            // url:'payment',
+            method: 'post',
+            data : {
+                amount : priceForStripe,
+                token
+            }
+        }).then((res) => {
+            alert('payment succesfull')
+            console.log(res);   
+        }).catch((error) => {
+            console.log('there was an error',JSON.parse(error));
+            alert('There was an error in your payment, please use provided credit card details')
+        })
     }
 
     return (
